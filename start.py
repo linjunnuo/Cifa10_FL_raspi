bab62826b5e2e6609b3300bd2c56ca05ffc675d7
linjunnuo <limchvnno@gmail.com>
2022-10-06 13:11:22 +0800

    Initial commit

---
+++ start.py
@@ -0,0 +1,11 @@
+import paramiko
+usr = 8
+
+for i in range(usr):
+    ip = '192.168.1.1'+str(i+11)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=ip, port=22, username='pi', password='ljnnnnnn')
+    stdin, stdout, stderr = ssh.exec_command('python /home/pi/Desktop/client_rasp_test.py')
+
+
