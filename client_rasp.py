a34d5afed33f47e091287650c8ca71b1f963f93d
linjunnuo <limchvnno@gmail.com>
2022-10-07 16:27:02 +0800

    update in 2022/10/07 16:26

--- client_rasp.py
+++ client_rasp.py
@@ -1,42 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.9
+
 # coding: utf-8
-
-# # CIFAR10 Federated Mobilenet Client Side
-# This code is the server part of CIFAR10 federated mobilenet for **multi** client and a server.
-
-# In[3]:
-
-
-users = 8 # number of clients
-
-
-# In[4]:
-
 
 import os
 import h5py
-
 import socket
 import struct
 import pickle
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
-
 from torch.utils.data import Dataset, DataLoader
 from netifaces import ifaddresses, AF_INET
 import time
-
 from tqdm import tqdm
+from gpiozero import CPUTemperature
+from torch.utils.data import Subset
 
 
-
-# In[2]:
-
+users = 8 # number of clients
+batch_size = 32 # batch size
+rounds = 40 # client-server communication rounds
+local_epochs = 1 # local epoch
+host = "192.168.1.104" # Set host address
+port = 2000 #Set port number
+max_recv = 100000
 
 def getFreeDescription():
     free = os.popen("free -h")
@@ -47,7 +38,6 @@ def getFreeDescription():
         if i == 1:
             return (line.split()[0:7])
 
-
 def getFree():
     free = os.popen("free -h")
     i = 0
@@ -56,9 +46,6 @@ def getFree():
         line = free.readline()
         if i == 2:
             return (line.split()[0:7])
-
-from gpiozero import CPUTemperature
-
 
 def printPerformance():
     cpu = CPUTemperature()
@@ -75,122 +62,47 @@ def printPerformance():
     print(description[4] + " : " + mem[5])
     print(description[5] + " : " + mem[6])
 
-
-# In[3]:
-
-
 printPerformance()
 
-
-# In[5]:
-
-
-root_path = '../../models/cifar10_data'
-
-
-# ## Cuda
-
-# In[6]:
-
-
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Cuda：
 device = "cpu"
-print(device)
 
-
-# In[7]:
-
+# Set the codename of the client according to the IP address：
 client_order = int(ifaddresses('wlan0')[AF_INET][0]['addr'][11:13])-11
 
-
-
-# In[8]:
-
-
+# The amount of data each client needs to compute：
 num_traindata = int(50000/users)
 
-
-
-# ## Data load
-
-# In[9]:
-
-
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])
-
-from torch.utils.data import Subset
-
-
+# load Data：
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))]) # Normalize
 indices = list(range(50000))
-
 part_tr = indices[num_traindata * client_order : num_traindata * (client_order + 1)]
-
-
-
-
-# In[10]:
-batch_size = 32
-
 trainset = torchvision.datasets.CIFAR10 (root='/home/pi/Desktop/dataset', train=True, download=False, transform=transform)
-
-trainset_sub = Subset(trainset, part_tr)
-
+trainset_sub = Subset(trainset, part_tr) # Let each client train with different data
 train_loader = torch.utils.data.DataLoader(trainset_sub, batch_size=batch_size, shuffle=True, num_workers=2)
-
 testset = torchvision.datasets.CIFAR10 (root='/home/pi/Desktop/dataset', train=False, download=False, transform=transform)
 test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
-
-
-# In[11]:
-
-
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-# ### Number of total batches
-
-# In[13]:
-
 
 train_total_batch = len(train_loader)
 print(train_total_batch)
 test_batch = len(test_loader)
 print(test_batch)
 
-
-# ## Pytorch layer modules for *Conv1D* Network
-# 
-# 
-# 
-# ### `Conv1d` layer
-# - `torch.nn.Conv1d(in_channels, out_channels, kernel_size)`
-# 
-# ### `MaxPool1d` layer
-# - `torch.nn.MaxPool1d(kernel_size, stride=None)`
-# - Parameter `stride` follows `kernel_size`.
-# 
-# ### `ReLU` layer
-# - `torch.nn.ReLU()`
-# 
-# ### `Linear` layer
-# - `torch.nn.Linear(in_features, out_features, bias=True)`
-# 
-# ### `Softmax` layer
-# - `torch.nn.Softmax(dim=None)`
-# - Parameter `dim` is usually set to `1`.
-
-# In[14]:
-
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  1 14:23:31 2018
-@author: tshzzz
-"""
-
-import torch
-
-
+# CNN:
+# Model(
+#   (model): Sequential(
+#     (0): Conv2d(3, 32, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
+#     (1): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (2): Conv2d(32, 32, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
+#     (3): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (4): Conv2d(32, 64, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
+#     (5): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+#     (6): Flatten(start_dim=1, end_dim=-1)
+#     (7): Linear(in_features=1024, out_features=64, bias=True)
+#     (8): Linear(in_features=64, out_features=10, bias=True)
+#   )
+# )
 class MobileNet(nn.Module):
     def __init__(self):
         super(MobileNet, self).__init__()
@@ -209,32 +121,13 @@ class MobileNet(nn.Module):
     def forward(self, x):
         x = self.model(x)
         return x
-
-
-# In[15]:
-
-
 mobile_net = MobileNet()
 mobile_net.to(device)
-
-
-# In[16]:
-
-
 lr = 0.001
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(mobile_net.parameters(), lr=lr, momentum=0.9)
 
-rounds = 40 # default
-local_epochs = 1 # default
-
-
-# ## Socket initialization
-# ### Required socket functions
-
-# In[17]:
-
-
+# Socket initialization
 def send_msg(sock, msg):
     # prefix each message with a 4-byte length in network byte order
     msg = pickle.dumps(msg)
@@ -262,53 +155,18 @@ def recvall(sock, n):
         data += packet
     return data
 
-
-# In[15]:
-
-
 printPerformance()
 
-
-# ### Set host address and port number
-
-# In[18]:
-
-
-host = "192.168.1.104"
-port = 2000
-max_recv = 100000
-
-
-# ### Open the client socket
-
-# In[19]:
-
-
+# Open the client socket
 s = socket.socket()
 s.connect((host, port))
-
-
-# ## SET TIMER
-
-# In[20]:
-
-
 start_time = time.time()    # store start time
 print("timmer start!")
-
-
-# In[21]:
-
-
 msg = recv_msg(s)
 rounds = msg['rounds'] 
 client_id = msg['client_id']
 local_epochs = msg['local_epoch']
 send_msg(s, len(trainset_sub))
-
-
-# In[22]:
-
 
 # update weights from server
 # train
@@ -338,23 +196,6 @@ for r in range(rounds):  # loop over the dataset multiple times
     send_msg(s, msg)
 
 print('Finished Training')
-
-
-# In[ ]:
-
-
 printPerformance()
-
-
-# In[23]:
-
-
 end_time = time.time()  #store end time
 print("Training Time: {} sec".format(end_time - start_time))
-
-
-# In[ ]:
-
-
-
-
--- start.py
+++ start.py
@@ -1,5 +1,4 @@
 import paramiko
-from time import sleep
 usr = 8
 
 for i in range(usr):
@@ -8,20 +7,3 @@ for i in range(usr):
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname=ip, port=22, username='pi', password='ljnnnnnn')
     stdin, stdout, stderr = ssh.exec_command('python /home/pi/Desktop/client_rasp.py.py')
-
-# from fabric import Connection
-# usr = 8
-# # 建议将ssh连接所需参数变量化
-# user = 'pi'
-# password = 'ljnnnnnn'
-# for i in range(usr):
-#     host = '192.168.1.1'+str(i+11)
-#     # 利用fabric.Connection快捷创建连接
-#     c = Connection(host=f'{user}@{host}',
-#                    connect_kwargs=dict(
-#                        password=password
-#                    ))
-#
-#     # 利用run方法直接执行传入的命令
-#     c.run('python /home/pi/Desktop/client_rasp.py', hide=True)
-
